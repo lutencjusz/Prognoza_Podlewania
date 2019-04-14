a67dce6da7af5bfc9fdaa51c57ea2e21a38241d1
lutencjusz <lutencjusz@gmail.com>
2019-04-14 14:28:02 +0200

    commit

---
+++ prognoza.bat
@@ -0,0 +1,7 @@
+start node pobierzPrognozePogody.js // pobiera pogode z serwisu openweathermap
+start node pobierzParametry.js // pobiera parametry z pliku parametry.json
+start node ustawAlertyPocz.js // ustawia pocz¹tkowe wartoœci tablicy alertów
+start node czyTemperatura.js // uruchamia logikê dotycz¹c¹ temperatury
+start node czyWilgotnosc.js // uruchamia logikê dotycz¹c¹ wilgotnoœci
+start node pokazAlarmy.js // zapisuje alerty do pliku alert.json i udostêpnia w serwisie RESTFul alerty i parametry
+
